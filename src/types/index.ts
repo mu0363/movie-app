9390b7635f0522606d8b2c158a6e40a93ec639d3
mu0363 <dde036@gmail.com>
2022-01-30 20:27:44 +0900

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+import { Session, Provider, User } from "@supabase/supabase-js";
+import { ChangeEvent } from "react";
+
+export type DatabaseUser = {
+  avatar_url: string;
+  created_at: string;
+  email: string;
+  full_name: string;
+  id: string;
+  updated_at: Date;
+};
+
+export type UpdateUserData = {
+  id: string;
+  full_name?: string;
+  avatar_url?: string;
+  updated_at: Date;
+};
+
+export type UserContextType = {
+  session: Session;
+  user: DatabaseUser;
+  signIn: () => Promise<{
+    session: Session | null;
+    user: User | null;
+    provider?: Provider;
+    url?: string | null;
+    error: Error | null;
+    data: Session | null;
+  }>;
+  signOut: () => void;
+  updateFullName: (updateUserData: UpdateUserData) => void;
+  updateAvatar: (event: ChangeEvent<HTMLInputElement>) => void;
+};
--- src/types/types.ts
+++
@@ -1,5 +0,0 @@
-export type UserDetails = {
-  id: string /* primary key */;
-  full_name?: string;
-  avatar_url?: string;
-};
